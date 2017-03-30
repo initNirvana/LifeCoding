352eb5989dbe3d2e9f7442e16f7f5455add4a143
Nirvana <kimqqyun@gmail.com>
2017-03-31 06:05:19 +0900

    deleted pyc, updated git ignore

--- Euler/Problem3.py
+++ Euler/Problem3.py
@@ -1,14 +1,9 @@
-import itertools
-def factor(n):
-	x = 2
-	while n > x:
-		if n % x == 0:
-			while n % x == 0:
-				n /= x
-			else:
-				yield x
-		x += x % 2 + 1
-	else:
-		yield x
+from __future__ import print_function
+def largest_prime_factor(n):
+    d1 = next (x for x in range(2,n+1) if (n % x ==0))
+    if n == d1:
+        return n
+    else:
+        return max(largest_prime_factor(d1),largest_prime_factor(int(n/d1)))
 
-print max(list(factor(600851475143)))
+print(largest_prime_factor(600851475143))
